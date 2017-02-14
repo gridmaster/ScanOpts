d0e6b29397af5786b75a8e4b84b8044cf35a6f00
Jim <AntlerHorse@gmail.com>
2017-02-14 08:17:21 -0500

    latest code

---
+++ ORMService/Models/OptionRepo.cs
@@ -0,0 +1,14 @@
+﻿using ORMService.Contracts;
+//using ScanOpts.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORMService.Models
+{
+    public class OptionRepo //: IRepository<Option>
+    {
+    }
+}
