7df3dce41ad1b02a38059bc00faafda4d6a522b2
Jim <AntlerHorse@gmail.com>
2017-06-18 14:06:39 -0400

    missing files

--- Services/BollingerBandsService.cs
+++ Services/BollingerBandsService.cs
@@ -3,10 +3,101 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Core;
+using Core.Interface;
+using Core.ORMModels;
+using Core.JsonKeyStatistics;
+using ORMService;
 
 namespace Services
 {
-    class BollingerBands
+    public class BollingerBandsService : BaseService
     {
+        #region Private properties
+        private bool success;
+        private SymbolsORMService symbolORMService = new SymbolsORMService();
+        private ExchangeORMService exchangeORMService = new ExchangeORMService();
+        //private BulkLoadInsiders bulkLoadInsiders = null;
+        #endregion Private properties
+
+        #region Constructors
+
+        public BollingerBandsService(ILogger logger, SymbolsORMService symbolORMService, ExchangeORMService exchangeORMService) // , BulkLoadInsiders bulkLoadInsiders)
+            : base(logger)
+        {
+            ThrowIfIsInitialized();
+            IsInitialized = true;
+            this.symbolORMService = symbolORMService;
+            this.exchangeORMService = exchangeORMService;
+            //this.bulkLoadInsiders = new BulkLoadInsiders(logger);
+        }
+
+        #endregion Constructors
+
+        #region Public Methods
+
+        public void RunBollingerBandsCheck()
+        {
+            logger.InfoFormat("RunKeyStatisticsCollection - GetSymbols");
+            List<string> symbols = symbolORMService.GetSymbols();
+            RunBollingerBandsCheck(symbols);
+        }
+
+        public void RunBollingerBandsCheck(List<Symbols> symbols)
+        {
+            List<string> syms = new List<string>();
+
+            for (int i = 0; i < symbols.Count; i++)
+            {
+                syms.Add(symbols[i].Symbol);
+            }
+            RunBollingerBandsCheck(syms);
+        }
+
+        public void RunBollingerBandsCheck(List<string> symbols)
+        {
+            logger.InfoFormat("Start - RunKeyStatisticsCollection");
+
+            string uriString = "https://query1.finance.yahoo.com/v8/finance/chart/{0}?formatted=true&crumb=8ajQnG2d93l&lang=en-US&region=US&period1=-252356400&period2=1488949200&interval=1d&events=div%7Csplit&corsDomain=finance.yahoo.com";
+
+            try
+            {
+                foreach (string symbol in symbols)
+                {
+                    List<CallPuts> allCallPuts = new List<CallPuts>();
+                    logger.InfoFormat("Get {0} page", symbol);
+
+                    string sPage = WebPage.Get(String.Format(uriString, symbol));
+
+                    if (sPage.Contains("(404) Not Found")) continue;
+                    if (sPage.Contains("(400) Bad Request")) continue;
+
+                    BaseObject.RootObject quoteSummary = JsonConvert.DeserializeObject<BaseObject.RootObject>(sPage);
+
+                    int newId = 0;
+
+                    //BulkLoadKeyStatistics(allCallPuts);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Fatal("RunKeyStatisticsCollection: {0}", ex);
+            }
+            finally
+            {
+                logger.Info("End - RunKeyStatisticsCollection");
+                logger.InfoFormat("{0}********************************************************************************{0}", Environment.NewLine);
+            }
+        }
+
+        public void LoadKeyStatisticsInfo()
+        {
+            string url = "https://query1.finance.yahoo.com/v10/finance/quoteSummary/CAT?formatted=true&crumb=0xiMyBSKbKe&lang=en-US&region=US&modules=defaultKeyStatistics%2CfinancialData%2CcalendarEvents&corsDomain=finance.yahoo.com";
+            string sPage = WebPage.Get(url);
+            BaseObject.RootObject bo = JsonConvert.DeserializeObject<BaseObject.RootObject>(sPage);
+        }
+
+        #endregion Public Methods
     }
 }
