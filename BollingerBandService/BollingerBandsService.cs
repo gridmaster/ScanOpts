9110bbcb880fc15a5b1aca4c6065162e9b986ce6
Jim Ballard <antlerhorse@gmail.com>
2017-06-27 18:21:16 -0400

    more bollinger band work

--- BollingerBandService/BollingerBandsService.cs
+++ BollingerBandService/BollingerBandsService.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathNet.Numerics.Statistics;
 using Newtonsoft.Json;
 using Core;
 using Core.Business;
@@ -131,24 +133,92 @@ namespace BollingerBandService
         private bool ThisIsAGoodCandidate(string symbol, List<DailyQuotes> quotesList)
         {
             bool result = false;
-            decimal adjClose = 0;
+            decimal adjClose = 0, x2adjClose = 0;
             BollingerBandsList bolbands = new BollingerBandsList();
             bolbands.BollingerBands = new List<BollingerBand>();
+            List<ClosingPrices> closingPrice = new List<ClosingPrices>();
+
+            string nums = "90.7, 92.9, 92.98, 91.8, 92.66, 92.68, 92.3, 92.77, 92.54, 92.95, 93.2, 91.07, 89.83, 89.74, 90.4, 90.74, 88.02, 88.09, 88.84, 90.78, 90.54, 91.39, 90.65";
+            string nums1 = "92.9, 92.98, 91.8, 92.66, 92.68, 92.3, 92.77, 92.54, 92.95, 93.2, 91.07, 89.83, 89.74, 90.4, 90.74, 88.02, 88.09, 88.84, 90.78, 90.54, 91.39, 90.65";
+            string nums2 = "92.98, 91.8, 92.66, 92.68, 92.3, 92.77, 92.54, 92.95, 93.2, 91.07, 89.83, 89.74, 90.4, 90.74, 88.02, 88.09, 88.84, 90.78, 90.54, 91.39, 90.65";
+            string nums3 = "91.8, 92.66, 92.68, 92.3, 92.77, 92.54, 92.95, 93.2, 91.07, 89.83, 89.74, 90.4, 90.74, 88.02, 88.09, 88.84, 90.78, 90.54, 91.39, 90.65";
+
+            var numb = nums.Split(',');
+            var numb1 = nums1.Split(',');
+            var numb2 = nums2.Split(',');
+            var numb3 = nums3.Split(',');
 
             for (int i = 0; i < 20; i++)
             {
-                adjClose += quotesList[i].UnadjClose.Value;
+                //adjClose += quotesList[i].UnadjClose.Value;
+                //x2adjClose += quotesList[i].UnadjClose.Value * quotesList[i].UnadjClose.Value;
+                
+                ClosingPrices cp = new ClosingPrices();
+
+                double doubleOut = 0;
+                // double.TryParse(quotesList[i].UnadjClose.ToString(), out doubleOut);
+                double.TryParse(numb[i].ToString(), out doubleOut);
+                cp.ClosingPrice = doubleOut;
+                closingPrice.Add(cp);
             }
 
+            //decimal mean = adjClose/20;
+            var wtf = StreamingStatistics.MeanStandardDeviation(closingPrice.Select(x => x.ClosingPrice));
+            //decimal variance = closingPrice.Select(x => x).StandardDeviation();
+
+            for (int i = 0; i < 20; i++)
+            {
+                ClosingPrices cp = new ClosingPrices();
+
+                double doubleOut = 0;
+                double.TryParse(numb1[i].ToString(), out doubleOut);
+                cp.ClosingPrice = doubleOut;
+                closingPrice.Add(cp);
+            }
+
+            var wtf1 = StreamingStatistics.StandardDeviation(closingPrice.Select(x => x.ClosingPrice));
+
+            for (int i = 0; i < 20; i++)
+            {
+                ClosingPrices cp = new ClosingPrices();
+
+                double doubleOut = 0;
+                double.TryParse(numb2[i].ToString(), out doubleOut);
+                cp.ClosingPrice = doubleOut;
+                closingPrice.Add(cp);
+            }
+
+            var wtf2 = StreamingStatistics.StandardDeviation(closingPrice.Select(x => x.ClosingPrice));
+
+            for (int i = 0; i < 20; i++)
+            {
+                ClosingPrices cp = new ClosingPrices();
+
+                double doubleOut = 0;
+                double.TryParse(numb3[i].ToString(), out doubleOut);
+                cp.ClosingPrice = doubleOut;
+                closingPrice.Add(cp);
+            }
+
+            var wtf3 = StreamingStatistics.StandardDeviation(closingPrice.Select(x => x.ClosingPrice));
+
+
+            var bb = new BollingerBand
+            {
+                Symbol = quotesList[20].Symbol,
+                Close = quotesList[20].Close.Value,
+                Date = UnixTimeConverter.UnixTimeStampToDateTime(quotesList[20].Timestamp.Value),
+                High = quotesList[20].High.Value,
+                Low = quotesList[20].Low.Value,
+                Open = quotesList[20].Open.Value,
+                SMA20 = adjClose/20,
+                
+            };
             bolbands.BollingerBands.Add(LoadBollingerBand(quotesList[20]));
 
             for (int i = 20; i < quotesList.Count; i++)
             {
                 adjClose += quotesList[i].UnadjClose.Value;
-                if (i > 19) // once we have at lest 20
-                { 
-
-                }
 
             }
 
@@ -172,6 +242,11 @@ namespace BollingerBandService
                 //Volume
             };
             return bb;
+        }
+
+        public class ClosingPrices
+        {
+            public double ClosingPrice { get; set; }
         }
 
         #endregion Private Methods
