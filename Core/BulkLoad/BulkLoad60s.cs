1b9a92bef9e0481cef172e86c16417c32b161925
gridmaster <AntlerHorse@Gmail.com>
2021-01-19 08:13:29 -0500

    more stuff done

---
+++ Core/BulkLoad/BulkLoad60s.cs
@@ -0,0 +1,73 @@
+﻿using Core.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.BulkLoad
+{
+    public class BulkLoad60s
+    {
+        private static readonly string[] ColumnNames = new string[] { "SymbolId", "Symbol", "Date", "High", "Low", "Close", "Volume",
+            "SMA60High", "SMA60Low", "SMA60Close", "Slope60High", "Slope60Low", "Slope60Close"};
+
+        //public BulkLoad60s(ILogger logger) : base(logger, ColumnNames)
+        //{
+        //}
+
+        //public DataTable LoadDataTableWithDailyHistory(IEnumerable<SlopeAndBBCounts> dStats, DataTable dt)
+        //{
+        //    foreach (var value in dStats)
+        //    {
+        //        var sValue = value.SymbolId + "^" + value.Symbol + "^" + value.Date + "^" + value.High + "^" +
+        //            value.Low + "^" + value.Close + "^" + value.Volume + "^" + value.SMA60High + "^" + value.SMA60Low + "^" + value.SMA60Close + value.Slope60High + "^" + value.Slope60Low + "^" + value.Slope60Close;
+        //        DataRow row = dt.NewRow();
+
+        //        row.ItemArray = sValue.Split('^');
+
+        //        dt.Rows.Add(row);
+        //    }
+
+        //    return dt;
+        //}
+
+        #region Implement IDisposable
+        private IntPtr nativeResource = Marshal.AllocHGlobal(100);
+        //More Info
+
+        // Dispose() calls Dispose(true)
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // NOTE: Leave out the finalizer altogether if this class doesn't 
+        // own unmanaged resources itself, but leave the other methods
+        // exactly as they are. 
+        ~BulkLoad60s()
+        {
+            // Finalizer calls Dispose(false)
+            Dispose(false);
+        }
+
+        // The bulk of the clean-up code is implemented in Dispose(bool)
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // free managed resources
+            }
+            // free native resources if there are any.
+            if (nativeResource != IntPtr.Zero)
+            {
+                Marshal.FreeHGlobal(nativeResource);
+                nativeResource = IntPtr.Zero;
+            }
+        }
+        #endregion Implement IDisposable
+    }
+}
