7a7daf8522a560b762c493ec56d4e443bf913adc
gridmaster <AntlerHorse@Gmail.com>
2021-01-19 16:17:58 -0500

    adding 60 day SMA code

--- Core/BulkLoad/BulkLoad60s.cs
+++ Core/BulkLoad/BulkLoad60s.cs
@@ -1,4 +1,5 @@
-﻿using Core.Interface;
+﻿using Core.BusinessModels;
+using Core.Interface;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -9,30 +10,31 @@ using System.Threading.Tasks;
 
 namespace Core.BulkLoad
 {
-    public class BulkLoad60s
+    public class BulkLoad60s : BaseBulkLoad, IDisposable
     {
         private static readonly string[] ColumnNames = new string[] { "SymbolId", "Symbol", "Date", "High", "Low", "Close", "Volume",
-            "SMA60High", "SMA60Low", "SMA60Close", "Slope60High", "Slope60Low", "Slope60Close"};
+            "SMA60High", "SMA60Low", "SMA60Close", "Slope60High", "Slope60Low", "Slope60Close", "Ratio60High", "Ratio60Low", "Ratio60Close"};
 
-        //public BulkLoad60s(ILogger logger) : base(logger, ColumnNames)
-        //{
-        //}
+        public BulkLoad60s(ILogger logger) : base(logger, ColumnNames)
+        {
+        }
 
-        //public DataTable LoadDataTableWithDailyHistory(IEnumerable<SlopeAndBBCounts> dStats, DataTable dt)
-        //{
-        //    foreach (var value in dStats)
-        //    {
-        //        var sValue = value.SymbolId + "^" + value.Symbol + "^" + value.Date + "^" + value.High + "^" +
-        //            value.Low + "^" + value.Close + "^" + value.Volume + "^" + value.SMA60High + "^" + value.SMA60Low + "^" + value.SMA60Close + value.Slope60High + "^" + value.Slope60Low + "^" + value.Slope60Close;
-        //        DataRow row = dt.NewRow();
+        public DataTable LoadDataTableWithDailyHistory(IEnumerable<SlopeAnd60sCounts> dStats, DataTable dt)
+        {
+            foreach (var value in dStats)
+            {
+                var sValue = value.SymbolId + "^" + value.Symbol + "^" + value.Date + "^" + value.High + "^" +
+                    value.Low + "^" + value.Close + "^" + value.Volume + "^" + value.SMA60High + "^" + value.SMA60Low + "^" + value.SMA60Close
+                    + value.Slope60High + "^" + value.Slope60Low + "^" + value.Slope60Close + value.Ratio60High + "^" + value.Ratio60Low + "^" + value.Ratio60Close;
+                DataRow row = dt.NewRow();
 
-        //        row.ItemArray = sValue.Split('^');
+                row.ItemArray = sValue.Split('^');
 
-        //        dt.Rows.Add(row);
-        //    }
+                dt.Rows.Add(row);
+            }
 
-        //    return dt;
-        //}
+            return dt;
+        }
 
         #region Implement IDisposable
         private IntPtr nativeResource = Marshal.AllocHGlobal(100);
---
+++ Core/BusinessModels/SlopeAnd60sCounts.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Core.BusinessModels
+{
+    public class SlopeAnd60sCounts
+    {
+        public int Id { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(60)]
+        public string Symbol { get; set; }
+
+        [Column(TypeName = "INT")]
+        public int SymbolId { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(60)]
+        public string Symbol { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Column(TypeName = "DATETIME")]
+        public DateTime Date { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(20)]
+        public string Exchange { get; set; }
+
+        public decimal? Open { get; set; }
+        public decimal? High { get; set; }
+        public decimal? Low { get; set; }
+        public decimal? Close { get; set; }
+        public decimal? Volume { get; set; }
+        public decimal? SMA60High { get; set; }
+        public decimal? SMA60Low { get; set; }
+        public decimal? SMA60Close { get; set; }
+        public decimal? Slope60High { get; set; }
+        public decimal? Slope60Low { get; set; }
+        public decimal? Slope60Close { get; set; }
+        public decimal? Ratio60High { get; set; }
+        public decimal? Ratio60Low { get; set; }
+        public decimal? Ratio60Close { get; set; }
+    }
+}
